7bae5a15da6187baed09981468d4996bacde5ed1
Dat Vo <dat.cit241@gmail.com>
2022-11-06 23:54:30 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,35 +7,36 @@ import RemoveIcon from '@mui/icons-material/Remove'
 import AddIcon from '@mui/icons-material/Add'
 import IconButton from '@mui/material/IconButton'
 const OpacitySlider = styled(Slider)({
-  color: '#52af77',
-  height: 8,
+  color: '#EB6017',
+  height: '6px',
   '& .MuiSlider-track': {
     border: 'none',
   },
   '& .MuiSlider-thumb': {
-    height: 24,
-    width: 24,
-    backgroundColor: '#52af77',
-    border: '2px solid #fff',
+    height: '28px',
+    width: '28px',
+    backgroundColor: '#EB6017',
+    border: '3px solid white',
     '&:focus, &.Mui-active, &.Mui-focusVisible, &:hover': {
       boxShadow: 'inherit',
     },
   },
   '& .MuiSlider-valueLabel': {
     fontSize: 12,
+    top: '24px',
     fontWeight: 'normal',
-    top: 22,
-    backgroundColor: 'unset',
-    color: '#fff',
+    backgroundColor: 'transparent',
+    color: 'white',
   },
   '& .MuiSlider-rail': {
-    opacity: 0.5,
-    backgroundColor: '#bfbfbf',
+    opacity: 0,
+    // backgroundColor: '#bfbfbf',
+    // backgroundColor: 'red'
   },
 })
 
 const OpacityButton = styled(IconButton)({
-  color: '#52af77',
+  // color: '#52af77',
   height: 23,
   width: 23,
   '&:hover, &.Mui-focusVisible': {
@@ -68,13 +69,20 @@ function App() {
           width: 300,
           m: 3,
           p: 1,
-          backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          backgroundColor: '#e8e8e8',
           boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.15)',
           borderRadius: '10px',
         }}
       >
-        <Stack spacing={1} direction='row' sx={{ mb: 1 }} alignItems='center'>
-          <OpacityButton aria-label='minus' onClick={handleMinusByOne}>
+        <Stack spacing={2} direction='row' sx={{ mb: 1 }} alignItems='center'>
+          <OpacityButton
+            aria-label='minus'
+            onClick={handleMinusByOne}
+            sx={{
+              color: value === 0 ? '#8C96A0' : '#EB6017',
+              backgroundColor: value === 0 ? '#EFEFEF' : 'rgba(235, 96, 23, 0.15)'
+            }}
+          >
             <RemoveIcon fontSize='small' />
           </OpacityButton>
 
@@ -84,7 +92,14 @@ function App() {
             value={value}
             onChange={handleSliderChange}
           />
-          <OpacityButton aria-label='plus' onClick={handlePlusByOne}>
+          <OpacityButton
+            aria-label='plus'
+            onClick={handlePlusByOne}
+            sx={{
+              color: value === 100 ? '#8C96A0' : '#EB6017',
+              backgroundColor: value === 100 ? '#EFEFEF' : 'rgba(235, 96, 23, 0.15)'
+            }}
+          >
             <AddIcon fontSize='small' />
           </OpacityButton>
         </Stack>
